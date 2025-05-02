29eb7ce1b0a14ebd2d0b3401bba52db462a65bed
wecandoitjapan <kazunori.fujimoto.1981@gmail.com>
2025-05-02 21:52:18 +0900

    会員側の有料プラン登録機能実装

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\Subscribed;
+use App\Http\Middleware\NotSubscribed;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\SubscriptionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +27,26 @@ Route::group(['middleware' => 'guest:admin'], function () {
 
     Route::group(['middleware' => ['auth', 'verified']], function () {
         Route::resource('user', UserController::class)->only(['index', 'edit', 'update']);
+
+        // まだサブスクリプションに加入していないユーザーだけがアクセスできるルート
+        Route::group(['middleware' => [NotSubscribed::class]], function () {
+            // 有料プラン登録ページを表示する。
+            Route::get('subscription/create', [SubscriptionController::class, 'create'])->name('subscription.create');
+            // 	ユーザーのサブスクリプションを作成する
+            Route::post('subscription', [SubscriptionController::class, 'store'])->name('subscription.store');
+        });
+
+        // すでにサブスクリプションに加入しているユーザーだけが利用できるルート
+        Route::group(['middleware' => [Subscribed::class]], function () {
+            // 加入中プランの編集フォームを表示
+            Route::get('subscription/edit', [SubscriptionController::class, 'edit'])->name('subscription.edit');
+            // 編集内容（例：プラン変更）を保存
+            Route::patch('subscription', [SubscriptionController::class, 'update'])->name('subscription.update');
+            // 解約の確認画面などを表示
+            Route::get('subscription/cancel', [SubscriptionController::class, 'cancel'])->name('subscription.cancel');
+            // 実際にサブスクリプションを解約
+            Route::delete('subscription', [SubscriptionController::class, 'destroy'])->name('subscription.destroy');
+        });
     });
 });
 
