532d83fe31fafe44c42a411a18ccc27d1cc8b85a
wecandoitjapan <kazunori.fujimoto.1981@gmail.com>
2025-05-08 22:19:32 +0900

    会員側の予約機能の実装

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -22,7 +22,15 @@ class Restaurant extends Model
         return $this->hasMany(Review::class);
     }
 
+    public function reservations() {
+        return $this->hasMany(Reservation::class);
+    }
+
     public function ratingSortable($query, $direction) {
         return $query->withAvg('reviews', 'score')->orderBy('reviews_avg_score', $direction);
+    }
+
+    public function popularSortable($query, $direction) {
+        return $query->withCount('reservations')->orderBy('reservations_count', $direction);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,6 +54,9 @@ Route::group(['middleware' => 'guest:admin'], function () {
             Route::delete('subscription', [SubscriptionController::class, 'destroy'])->name('subscription.destroy');
 
             Route::resource('restaurants.reviews', ReviewController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
+
+            Route::resource('reservations', ReservationController::class)->only(['index', 'destroy']);
+            Route::resource('restaurants.reservations', ReservationController::class)->only(['create', 'store']);
         });
     });
 });
